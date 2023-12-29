a8a5c3d662b5880dcbebde809dd5a058fa46e23f
Aditya-Padwal <aditya.padwal3102@gmail.com>
2023-12-29 22:39:43 +0530

    Basic chat system implemented!

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Websockets and Socket.io
+
+Learning Websockets in Node.js and Real-Time messaging using Socket.io
+
+## Tech Stack
+
+**Client:** Static HTML pages
+
+**Server:** Node, Express, Socket.io
---
+++ index.js
@@ -0,0 +1,38 @@
+// Importing modules
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const {Server} = require("socket.io");
+
+// Creating an instance of the express application
+const app = express();
+// Creating http server
+const server = http.createServer(app);
+
+// Creating a new Web-socket server
+const io = new Server(server);
+
+// Socket.io
+// Establishing a socket.io connection
+io.on("connection", (socket) => {
+    console.log("Server Web sockets connection established!");
+    // Receiving msgs from the client
+    socket.on("client-message", (message) => {
+        console.log("A new client message has arrived, ", message);
+        // After receiving msg from the client, server sends this msgs to all the clients
+        io.emit('message', message);
+    })
+})
+
+// Serving static files
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+    return res.sendFile("./public/index.html");
+});
+
+// Listening on port 9000
+server.listen(9000, () => {
+    console.log("Server started at port 8000");
+});
